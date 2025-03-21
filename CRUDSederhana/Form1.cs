e740d3c023a378e4ccc087600b0a633742a47468
galang48 <galangyudha1945@gmail.com>
2025-03-21 14:58:07 +0700

    Menambahkan fungsi refresh untuk memperbarui tampilan data mahasiswa

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -227,6 +227,14 @@ namespace CRUDSederhana
             }
         }
 
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadData();
+
+            // ✧ Debugging: Cek jumlah kolom dan baris
+            MessageBox.Show($"Jumlah Kolom: {dgvMahasiswa.ColumnCount}\nJumlah Baris: {dgvMahasiswa.RowCount}",
+                "Debugging DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
 
     }
 }
